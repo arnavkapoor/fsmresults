9f988ec0175bcdbaa44353ec92070e9e6e676938
Arnav Kapoor <arnavk805@gmail.com>
2018-06-27 21:00:42 +0530

    Minor error in allregexnew rectified

--- newbeginning/allregexnew/correct.sh
+++ newbeginning/allregexnew/correct.sh
@@ -1,6 +1,5 @@
 while IFS='' read -r line || [[ -n "$line" ]]; do
     nm=`echo "$line" | cut -d ' ' -f1`
     tc=`echo "$line" | cut -d ' ' -f2`
-    sed -i -e "3s/\.s/\.s $nm/g" $tc
-    echo $nm	
+    sed -i -r '3s/\S+//3' $tc
 done < "$1"

84ea8fc8fb15c54dfceb8daebd03cc9f27c83e54
Arnav Kapoor <arnavk805@gmail.com>
2018-06-27 18:52:34 +0530

    Added the results of the fsm-analysis

---
+++ newbeginning/network/fsmanalysis/scripts/fsmanalysis.py
@@ -0,0 +1,20 @@
+import os,csv
+totalnetwork= 255
+path = '/home/arnav/fsmresults/newbeginning/network/fsmanalysis/data/allregexnew'
+for folder, sub_folders, files in os.walk(path):
+    for special_file in files:
+        file_path = os.path.join(folder, special_file)
+        with open(file_path, 'r+') as read_file:
+            d=dict()
+            for line in read_file:
+                line = line.strip().split()
+                if line[1] not in d:
+                    d[line[1]] = 1
+                else:
+                    d[line[1]] += 1
+
+        filename=file_path.rsplit('/')[-1]
+        file = csv.writer(open('/home/arnav/fsmresults/newbeginning/network/fsmanalysis/data/results/' + filename + ".csv", 'w'))
+        file.writerow(['State', 'Transitions', 'Percentage Transitions'])
+        for key,values in d.items():
+            file.writerow([key,values,values/totalnetwork])

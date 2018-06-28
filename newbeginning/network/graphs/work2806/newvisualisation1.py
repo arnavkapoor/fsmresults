86f43277bb1eebffcea60f77853927debc00215d
Arnav Kapoor <arnavk805@gmail.com>
2018-06-28 19:30:01 +0530

    Added script for graph and sample graph for aim.csv

---
+++ newbeginning/network/graphs/work2806/newvisualisation1.py
@@ -0,0 +1,66 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly.figure_factory as FF
+import math
+import numpy as np
+import pandas as pd
+df = pd.read_csv('./individualcsv/aim.test.csv')
+
+testcases=(df['Testcases'].drop_duplicates().values.tolist())
+totalcpu = (df['Total CPU'].values.tolist())
+totalcpu32=[]
+totalcpu16=[]
+totalcpu8=[]
+totalcpu1=[]
+
+for i in range(0,len(totalcpu)):
+    if(i%4==0):
+        totalcpu32.append(totalcpu[i])
+    if(i%4==1): 
+        totalcpu16.append(totalcpu[i])
+    if(i%4==2):
+        totalcpu8.append(totalcpu[i])
+    if(i%4==3):
+        totalcpu1.append(totalcpu[i])
+
+names = ['cpu1','cpu8','cpu16','cpu32','gpuexec','gputotal']
+
+executiongpuop = (df['Execution GPU Optimised'].drop_duplicates().values.tolist())
+totalgpuop = (df['Total GPU Optimised'].drop_duplicates().values.tolist())
+allaboard = []
+print(len(testcases))
+for i in range(0,len(testcases)):
+    totalcpu1[i] = (testcases[i]/totalcpu1[i])*1000
+    totalcpu8[i] = (testcases[i]/totalcpu8[i])*1000
+    totalcpu16[i] = (testcases[i]/totalcpu16[i])*1000
+    totalcpu32[i] = (testcases[i]/totalcpu32[i])*1000
+    executiongpuop[i] = (testcases[i]/executiongpuop[i])*1000
+    totalgpuop[i] = (testcases[i]/totalgpuop[i])*1000
+
+allaboard.extend([totalcpu1,totalcpu8,totalcpu16,totalcpu32,executiongpuop,totalgpuop])
+print(allaboard)
+
+dataPanda=[]
+
+for i in range(0,len(allaboard)):
+    trace = go.Scatter(
+         x=testcases,
+         y=allaboard[i],
+         mode='lines+markers',
+         name=names[i]
+    )
+    dataPanda.append(trace)
+
+layout ={
+        'yaxis': {
+        'title' : 'Testcases/second'
+        },
+        'xaxis': {
+        'title' : 'Number of Testcases'
+        },
+ }
+
+fig = dict(data=dataPanda,layout=layout)
+py.image.save_as(fig, './individualgraphs/aimgraph.png')
+
+

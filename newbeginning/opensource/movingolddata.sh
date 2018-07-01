1dc16e24ad43f140c156ecb158b4757993e9d555
Arnav Kapoor <v1akapoo@swift.inf.ed.ac.uk>
2018-07-01 15:21:56 +0100

    copy for opensource

---
+++ newbeginning/opensource/movingolddata.sh
@@ -0,0 +1,4 @@
+git mv newrawdata/* computed_results/t1/
+git mv graphs/*.csv computed_results/t1/
+git mv graphs/individualgraphs/* computed_results/t1/
+git mv graphs/individualcsv/* computed_results/t1/

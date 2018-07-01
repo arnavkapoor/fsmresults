0254df44b015f81ea672ad7a11ac632113316b77
Arnav Kapoor <arnavk805@gmail.com>
2018-07-02 04:06:32 +0530

    ldim results

--- newbeginning/network/movingolddata.sh
+++ newbeginning/network/movingolddata.sh
@@ -2,3 +2,5 @@ mv newrawdata/* computed_results/testing_run_dense/
 mv graphs/*.csv computed_results/testing_run_dense/
 mv graphs/individualgraphs/* computed_results/testing_run_dense/individualgraphs
 mv graphs/individualcsv/* computed_results/testing_run_dense/individualcsv
+mv graphs/individualcsvldim/* computed_results/testing_run_dense/individualcsvldim
+mv graphs/individualgraphsldim/* computed_results/testing_run_dense/individualgraphsldim

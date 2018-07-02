e6b3e823dda091ff2ae45aed33ec57a27a9cf9d3
Arnav Kapoor <arnavk805@gmail.com>
2018-07-02 21:48:35 +0530

    updat maxlength

--- newbeginning/network/swiftsidecode/maxinputlength.py
+++ newbeginning/network/swiftsidecode/maxinputlength.py
@@ -1,20 +1,19 @@
 import os
 import re
 
-directory = os.fsencode("./transition-pair-tests-network")
+path = "./transition-pair-tests-network"
+for folder, sub_folders, files in os.walk(path):
+    for special_file in files:
+        file_path = os.path.join(folder, special_file)
+        with open(file_path, 'r+') as fsmfile:
+            alllines=fsmfile.readlines()
+            length=[]
+            for lines in alllines:
+                if len(lines.split(" ")) == 2: 
+                    length.append(len(lines.split(" ")[1])- (3 * len(re.findall(r"\\[0-9][0-9][0-9]", lines.split(" ")[1]))) - len(re.findall(r"\n", lines.split(" ")[1])) - len(re.findall(r"\t", lines.split(" ")[1])) - len(re.findall(r"\a", lines.split(" ")[1])) ) 
+                else:
+                    length.append(len(lines.split(" ")[0])- (3 * len(re.findall(r"\\[0-9][0-9][0-9]", lines.split(" ")[0]))) - len(re.findall(r"\n", lines.split(" ")[0])) - len(re.findall(r"\t", lines.split(" ")[0])) - len(re.findall(r"\a", lines.split(" ")[0])) )
+                    
 
-for file in os.listdir(directory):
-    filepath = os.path.join(directory, file)
-    with open(filepath,"r") as fsmfile:
-        #print(file.strip())
-        alllines=fsmfile.readlines()
-        length=[]
-        for lines in alllines:
-            if len(lines.split(" ")) == 2: 
-                length.append(len(lines.split(" ")[1])- (3 * len(re.findall(r"\\[0-9][0-9][0-9]", lines.split(" ")[1]))) - len(re.findall(r"\n", lines.split(" ")[1])) - len(re.findall(r"\t", lines.split(" ")[1])) - len(re.findall(r"\a", lines.split(" ")[1])) ) 
-            else:
-                length.append(len(lines.split(" ")[0])- (3 * len(re.findall(r"\\[0-9][0-9][0-9]", lines.split(" ")[0]))) - len(re.findall(r"\n", lines.split(" ")[0])) - len(re.findall(r"\t", lines.split(" ")[0])) - len(re.findall(r"\a", lines.split(" ")[0])) )
-                
-
-        length.sort()
-        print(length[len(length)-1])
+            length.sort()
+            print(length[len(length)-1])

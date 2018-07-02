8487a7c1c3d8987bb35e0c24b0184d6bc06a1866
Arnav Kapoor <arnavk805@gmail.com>
2018-07-02 17:17:33 +0530

    ldim results update for kernel times

---
+++ newbeginning/network/swiftsidecode/medianinputlength.py
@@ -0,0 +1,19 @@
+import os
+import re
+
+
+directory = os.fsencode("./transition-pair-tests-network")
+
+for file in os.listdir(directory):
+    filepath = os.path.join(directory, file)
+    with open(filepath,"r") as fsmfile:
+        print(file)
+        alllines=fsmfile.readlines()
+        length=[]
+        for lines in alllines:
+            if len(lines.split(" ")) == 2: 
+                length.append(len(lines.split(" ")[1])- (3 * len(re.findall(r"\\[0-9][0-9][0-9]", lines.split(" ")[1]))))
+            else:
+                length.append(len(lines.split(" ")[0])- (3 * len(re.findall(r"\\[0-9][0-9][0-9]", lines.split(" ")[0]))))
+        length.sort()
+        print(length[len(length)//2])
